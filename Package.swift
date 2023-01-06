894e5e50c4499ab8c8e504d95e20bba297d75237
jaroshevskii <jaroshevskiisasha@gmail.com>
2023-01-06 02:05:29 +0200

    Update

--- Package.swift
+++ Package.swift
@@ -1,19 +1,19 @@
-// swift-tools-version: 5.5
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-  name: "Repeat",
+  name: "RepeatCLI",
   dependencies: [
     // Dependencies declare other packages that this package depends on.
-    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.0")
+    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0")
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .executableTarget(
-      name: "repeat",
+      name: "repeat-cli",
       dependencies: [
         .product(name: "ArgumentParser", package: "swift-argument-parser")
       ])
---
+++ Sources/repeat-cli/RepeatCLI.swift
@@ -0,0 +1,48 @@
+//
+// Repeat CLI
+//
+
+import ArgumentParser
+
+@main
+struct RepeatCLI: ParsableCommand {
+  static var configuration = CommandConfiguration(
+    abstract: "Command-line text repeater.",
+    version: """
+    Repeat CLI version 0.1.0
+      GitHub: https://github.com/jaroshevskii/repeat-cli
+    """
+  )
+
+  /// Text to repeat.
+  @Argument(help: "Text to repeat.")
+  var text: String
+
+  /// Number of `text` repetitions.
+  @Option(
+    name: [.customShort("c"), .customLong("count")],
+    parsing: .unconditional,
+    help: "Number of 'text' repetitions.")
+  var repeatCount: Int = 2
+
+  /// Include a counter with each repetition.
+  @Flag(name: .shortAndLong, help: "Include a counter withc earh repetition.")
+  var includeCounter = false
+
+  /// Validates the properties of the instance after parsing.
+  func validate() throws {
+    guard repeatCount > 0 else {
+      throw ValidationError("'count' must be greater than zero.")
+    }
+  }
+
+  func run() {
+    for i in 1...repeatCount {
+      if includeCounter {
+        print("\(i): \(text)")
+      } else {
+        print(text)
+      }
+    }
+  }
+}
